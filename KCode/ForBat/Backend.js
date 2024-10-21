8966d69f2a7e0d10f34d5df0789dbb3aaa3733d2
keshavsoft <nknnkumar@yahoo.com>
2024-10-21 10:13:06 +0530

    kcode touched

--- KCode/ForBat/Backend.js
+++
@@ -1,6 +0,0 @@
-import { StartFunc as StartFuncReadDataSchema } from "./ReadDataSchema.js";
-import { StartFunc as StartFuncBackend } from "./CrudGenerator/Backend.js";
-
-let LocalFilesArray = StartFuncReadDataSchema();
-
-StartFuncBackend({ inFilesArray: LocalFilesArray });
--- KCode/ForBat/Database.js
+++
@@ -1,8 +0,0 @@
-import { StartFunc as StartFuncReadDataSchema } from "./ReadDataSchema.js";
-import { StartFunc as StartFuncCrudGenerator } from "../CrudGenerator/Database.js";
-
-let LocalFilesArray = StartFuncReadDataSchema();
-// console.log("LocalFilesArray : ", LocalFilesArray);
-
-StartFuncCrudGenerator({ inFilesArray: LocalFilesArray });
-// console.log("aaaaaaaaaa : ", LocalFilesArray);
