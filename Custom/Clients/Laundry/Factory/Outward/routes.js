eb9c05291cabe95495ba388366e3735a239bdd90
Srivasu215 <sri.vasu215@gmail.com>
2024-10-22 11:22:34 +0530

    CUSTOM LAUNDRY

--- Custom/Clients/Laundry/Factory/Outward/routes.js
+++ Custom/Clients/Laundry/Factory/Outward/routes.js
@@ -3,12 +3,12 @@ import express from 'express';
 var router = express.Router();
 
 import { router as routerFromScan } from './Scan/routes.js';
-// import { router as routerToBranch } from './ToBranch/routes.js';
+import { router as routerToBranch } from './ToBranch/routes.js';
 // import { router as routerFromReturn } from './Return/routes.js';
 //import { router as routerFromQrCodes } from './QrCodes/routes.js';
 
 router.use('/Scan', routerFromScan);
-// router.use('/ToBranch', routerToBranch);
+router.use('/ToBranch', routerToBranch);
 // router.use('/Return', routerFromReturn);
 //router.use('/QrCodes', routerFromQrCodes);
 
