287ddab3ce13e1354abe2f7436624e57d3ce1e5c
Srivasu215 <sri.vasu215@gmail.com>
2024-10-23 16:34:43 +0530

    CUSTOM fROM bRANCH

--- BatchFiles/ForLaundry/FrontEndMazerVite.bat
+++ BatchFiles/ForLaundry/FrontEndMazerVite.bat
@@ -1,6 +1,16 @@
 @echo off
 cd ..\FrontEndMazerVite
 
+call npm run FromBranchVoucher
+call npm run FromBranchQrCodes
+
 call npm run WashingVoucher
+call npm run FromEntryQrCodes
+
+call npm run PressingQrCodes
+call npm run PressingVoucher
+
+call npm run CompletionVoucher
+call npm run CompletionQrCodes
 
 xcopy .\publicDir\WashingVoucher ..\CrudGenV2\public\Factory\WashingVoucher /h /i /c /k /e /r /y
