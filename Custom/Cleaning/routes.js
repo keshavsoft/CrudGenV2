047e8a9e1baea68a1022dae08269f2ae48f7f766
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-10-25 23:19:09 +0530

    new dashboard end point

--- Custom/Cleaning/routes.js
+++ Custom/Cleaning/routes.js
@@ -4,8 +4,10 @@ var router = express.Router();
 
 import { router as Today } from './Today/routes.js';
 import { router as Branch } from './Branch/routes.js';
+import { router as Dasboard } from './Dasboard/routes.js';
 
 router.use('/Today', Today);
 router.use('/Branch', Branch);
+router.use('/Dasboard', Dasboard);
 
 export { router };
