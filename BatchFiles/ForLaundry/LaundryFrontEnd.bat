194b114c723a1bd8526e5c03287194b8ac33e503
keshavsoft <nknnkumar@yahoo.com>
2024-10-30 17:22:44 +0530

    LaundryFrontEnd.bat 19, 20 added

--- BatchFiles/ForLaundry/LaundryFrontEnd.bat
+++ BatchFiles/ForLaundry/LaundryFrontEnd.bat
@@ -16,7 +16,8 @@ call npm run Today-Orders
 call npm run Admin-AdminToday-QrCodes
 call npm run Admin-AdminToday-Orders
 call npm run Admin-Masters
-call npm run Admin-AdminAllDays-QrCodes
+call npm run Admin-AdminAllDays-QrCodeWise-AsArray
+call npm run Admin-AdminAllDays-QrCodeWise-BranchWise
 
 xcopy .\publicDir ..\CrudGenV2\public\Laundry /h /i /c /k /e /r /y
 
