56a73bccf3a63ae8fce059bad3c4388fe5dc6eec
keshavsoft <nknnkumar@yahoo.com>
2024-10-30 16:40:08 +0530

    branchwise working good

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/routes.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/routes.js
@@ -3,7 +3,9 @@ import express from 'express';
 var router = express.Router();
 
 import { router as AllBranches } from './AllBranches/routes.js';
+import { router as BranchWise } from './BranchWise/routes.js';
 
 router.use('/AllBranches', AllBranches);
+router.use('/BranchWise', BranchWise);
 
 export { router };
