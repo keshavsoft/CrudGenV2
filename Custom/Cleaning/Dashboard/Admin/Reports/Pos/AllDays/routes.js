6070a9e178d389908f3b61f1f6d5555b3c33ce0a
keshavsoft <nknnkumar@yahoo.com>
2024-10-30 11:36:40 +0530

    routes.js fixed for branchwise

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/routes.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/routes.js
@@ -3,9 +3,7 @@ import express from 'express';
 var router = express.Router();
 
 import { router as AllBranches } from './AllBranches/routes.js';
-import { router as BranchWise } from './BranchWise/routes.js';
 
 router.use('/AllBranches', AllBranches);
-router.use('/BranchWise', BranchWise);
 
 export { router };
