86b479fd4a6dd9cd3558b9035f888888cbffcc83
keshavsoft <nknnkumar@yahoo.com>
2024-11-14 10:38:47 +0530

    qrcode started

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/routes.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/routes.js
@@ -6,12 +6,12 @@ import { router as AllBranches } from './AllBranches/routes.js';
 import { router as BranchWise } from './BranchWise/routes.js';
 import { router as BranchFilter } from './BranchFilter/routes.js';
 import { router as Orders } from './Orders/routes.js';
-
+import { router as QrCodes } from './QrCodes/routes.js';
 
 router.use('/AllBranches', AllBranches);
 router.use('/BranchWise', BranchWise);
 router.use('/BranchFilter', BranchFilter);
 router.use('/Orders', Orders);
-
+router.use('/QrCodes', QrCodes);
 
 export { router };
