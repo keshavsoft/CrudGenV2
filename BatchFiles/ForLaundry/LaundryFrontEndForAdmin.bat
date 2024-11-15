4ddf4a9fe1fc9813ad1d830c7916f034c32f20ce
keshavsoft <nknnkumar@yahoo.com>
2024-11-15 11:41:19 +0530

    branchfilter in bat

--- BatchFiles/ForLaundry/LaundryFrontEndForAdmin.bat
+++ BatchFiles/ForLaundry/LaundryFrontEndForAdmin.bat
@@ -3,6 +3,7 @@ cd ..\LaundryFrontEndForAdmin
 
 call npm run AdminAllDays-CommonConfig-QrCode-AsArray
 call npm run AdminAllDays-CommonConfig-QrCode-BranchWise
+call npm run AdminAllDays-CommonConfig-QrCode-BranchFilter
 
 xcopy .\publicDir ..\CrudGenV2\public\Laundry\Admin /h /i /c /k /e /r /y
 
