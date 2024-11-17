956f2c7faca476d9afdb330346a4288e4b31a5d3
Santhosh1720 <santhoshreddy5656@gmail.com>
2024-11-17 11:01:16 +0530

    sh files created for LaundryFrontEndForAdmin LaundryFrontEndForBranch LaundryFrontEndForFactory

---
+++ BatchFiles/ForLaundry/LaundryFrontEndForAdmin.sh
@@ -0,0 +1,14 @@
+
+cd ../LaundryFrontEndForAdmin
+
+npm run Menu
+
+npm run AdminAllDays-CommonConfig-Menu
+
+npm run AdminAllDays-CommonConfig-QrCode-AsArray
+npm run AdminAllDays-CommonConfig-QrCode-BranchWise
+npm run AdminAllDays-CommonConfig-QrCode-BranchFilter
+
+cp -r  ./publicDir ../CrudGenV2/public/Laundry/Admin /h /i /c /k /e /r /y
+
+cd ../CrudGenV2
---
+++ BatchFiles/ForLaundry/LaundryFrontEndForBranch.sh
@@ -0,0 +1,15 @@
+
+cd ../LaundryFrontEndForBranch
+
+ npm run Today-Orders
+ npm run Today-QrCodes
+ npm run NewOrders
+ npm run CustomersToOrders
+ npm run Masters
+ npm run BranchToday
+ npm run TodayReports
+ npm run ToFactory
+
+cp -r  ./publicDir ../CrudGenV2/public/Laundry/Branch /h /i /c /k /e /r /y
+
+cd ../CrudGenV2
