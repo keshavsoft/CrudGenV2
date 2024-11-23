2ebed5a01137674a89b8e75c424b2db3137d3c96
sharath0036 <ksharathbabu2@gmail.com>
2024-11-23 17:49:39 +0530

    links for new ui

--- BatchFiles/ForLaundry/LaundryFrontEndForBranch.bat
+++ BatchFiles/ForLaundry/LaundryFrontEndForBranch.bat
@@ -13,6 +13,8 @@ call npm run Dashboard-QrCodeWise-AsArray
 call npm run Menu
 call npm run Branch-CommonConfig-Menu
 call npm run Dashboard-CommonConfig-AsArray
+call npm run FromFactory
+call npm run FromFactoryQrCodes
 
 
 xcopy .\publicDir ..\CrudGenV2\public\Laundry\Branch /h /i /c /k /e /r /y
