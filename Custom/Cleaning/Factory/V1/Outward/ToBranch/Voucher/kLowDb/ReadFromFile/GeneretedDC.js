f43e4e737ffa829eb7e3925430f2c2131f57e185
Srivasu215 <sri.vasu215@gmail.com>
2024-11-23 17:06:11 +0530

    OutwardDc

--- Custom/Cleaning/Factory/V1/Outward/ToBranch/Voucher/kLowDb/ReadFromFile/GeneretedDC.js
+++ Custom/Cleaning/Factory/V1/Outward/ToBranch/Voucher/kLowDb/ReadFromFile/GeneretedDC.js
@@ -1,12 +1,21 @@
 import { StartFunc as FactoryOut_DC } from "../../../../../../../../../binV4/FactoryOut_DC/CommonPull/kLowDb/PullData/returnAsArray.js";
+import { StartFunc as FactoryOut_QrCodeScan } from "../../../../../../../../../binV4/FactoryOut_QrCodeScan/CommonPull/kLowDb/PullData/returnAsArray.js";
 
 let StartFunc = ({ inId }) => {
-
     let LocalId = inId;
-    let LocalReturnData = {};
-    LocalReturnData = FactoryOut_DC();
+    let LocalFactoryOutDc = FactoryOut_DC();
+    let LocalFactoryScan = FactoryOut_QrCodeScan();
+    let LocalFiterDcData = LocalFactoryOutDc.filter(element => element.RefDC == LocalId);
 
-    return LocalReturnData.filter(element => element.VoucherRef == LocalId);
+    return jfMergeData({ inDc: LocalFiterDcData, inQr: LocalFactoryScan });
 };
+
+const jfMergeData = ({ inDc, inQr }) => {
+    return inDc.map(element => {
+        element.QrCount = inQr.filter(qr => qr.RefDC == element.VoucherRef).length;
+        return element;
+    });
+
+}
 
 export { StartFunc };
