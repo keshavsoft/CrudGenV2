319327afc77b2b5d8f780c0fc2eb287e0fb085e1
pushpadamalanka82 <y*********@gmail.com>
2024-11-27 16:01:24 +0530

    batchfile

--- BatchFiles/ForLaundry/LaundryFrontEndForBranch.bat
+++ BatchFiles/ForLaundry/LaundryFrontEndForBranch.bat
@@ -8,14 +8,14 @@ call npm run CustomersToOrders
 call npm run Masters
 call npm run BranchToday
 call npm run TodayReports
-call npm run ToFactory
 call npm run Dashboard-QrCodeWise-AsArray
 call npm run Menu
 call npm run Branch-CommonConfig-Menu
 call npm run Dashboard-CommonConfig-AsArray
-call npm run FromFactory
 call npm run FromFactoryQrCodes
-
+call npm run FactoryMenu
+call npm run Factory-ToFactory
+call npm run Factory-FromFactory
 
 xcopy .\publicDir ..\CrudGenV2\public\Laundry\Branch /h /i /c /k /e /r /y
 
