5a9c250e5c8f5dd16e8fc67d89de7af65c81bf2c
pushpadamalanka82 <y*********@gmail.com>
2024-11-27 11:41:46 +0530

    ENDPOINTS

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodesAtStage/routes.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodesAtStage/routes.js
@@ -3,11 +3,11 @@ import express from 'express';
 var router = express.Router();
 
 import { router as AllBranches } from './AllBranches/routes.js';
-// import { router as BranchWise } from './BranchWise/routes.js';
-// import { router as BranchFilter } from './BranchFilter/routes.js';
+import { router as BranchWise } from './BranchWise/routes.js';
+import { router as BranchFilter } from './BranchFilter/routes.js';
 
 router.use('/AllBranches', AllBranches);
-// router.use('/BranchWise', BranchWise);
-// router.use('/BranchFilter', BranchFilter);
+router.use('/BranchWise', BranchWise);
+router.use('/BranchFilter', BranchFilter);
 
 export { router };
