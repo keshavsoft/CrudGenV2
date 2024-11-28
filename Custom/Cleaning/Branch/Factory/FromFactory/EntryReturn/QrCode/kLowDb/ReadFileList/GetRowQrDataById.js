86d0b248b456e2b32c5a77046bc75f1215b08589
Srivasu215 <sri.vasu215@gmail.com>
2024-11-28 18:09:32 +0530

    Fron Cancel ReTurn Qr

--- Custom/Cleaning/Branch/Factory/FromFactory/EntryReturn/QrCode/kLowDb/ReadFileList/GetRowQrDataById.js
+++ Custom/Cleaning/Branch/Factory/FromFactory/EntryReturn/QrCode/kLowDb/ReadFileList/GetRowQrDataById.js
@@ -1,15 +1,12 @@
-import { StartFunc as QrCodes } from '../CommonFuncs/QrCodes.js';
-import { StartFunc as EntryScan } from '../CommonFuncs/EntryScan.js';
+import { StartFunc as QrCodes } from '../CommonFuncs/FromApi/QrCodes.js';
+import { StartFunc as EntryScan } from '../CommonFuncs/FromApi/FromFactoryCancelScan.js';
 
 let StartFunc = ({ inId }) => {
     let LocalId = inId;
     let LocalReturnData = { KTF: false };
 
     const Qrdb = QrCodes();
-    Qrdb.read();
-
     const EntryScandb = EntryScan();
-    EntryScandb.read();
 
     let LocalBranchScanFilter = EntryScandb.data.find(e => e.QrCodeId == LocalId);
 
