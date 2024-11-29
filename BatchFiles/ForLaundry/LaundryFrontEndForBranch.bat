4c1d022b208160767ba2ba957581efbed80605e2
sharath0036 <ksharathbabu2@gmail.com>
2024-11-29 17:07:22 +0530

    links for fromfactory qrcodes

--- BatchFiles/ForLaundry/LaundryFrontEndForBranch.bat
+++ BatchFiles/ForLaundry/LaundryFrontEndForBranch.bat
@@ -18,6 +18,10 @@ call npm run Factory-ToFactory
 call npm run FromFactory-Completion
 call npm run FromFactory-EntryReturn
 call npm run FromFactory-WashingReturn
+call npm run FromFactory-EntryReturnQrCodes
+call npm run FromFactory-WashingReturnQrCodes
+call npm run FromFactory-CompletionReturnQrCodes
+
 
 xcopy .\publicDir ..\CrudGenV2\public\Laundry\Branch /h /i /c /k /e /r /y
 
