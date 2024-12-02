16bbc9660b3c24671c5b0b2b486bfba5ac3c225e
sharath0036 <ksharathbabu2@gmail.com>
2024-12-02 18:44:08 +0530

    washing end point bug fixed

--- Custom/Cleaning/Factory/V1/Process/Washing/Return/QrCode/kLowDb/WriteToFile/FacoryEntryScan/Check/CheckBrcnchScan.js
+++ Custom/Cleaning/Factory/V1/Process/Washing/Return/QrCode/kLowDb/WriteToFile/FacoryEntryScan/Check/CheckBrcnchScan.js
@@ -6,21 +6,15 @@ const StartFunc = ({ inTable, inQrCodeId }) => {
 
     let LocalReturnData = { KTF: false };
     const dbForQrCodes = StartFuncCommonFuncs();
-    dbForQrCodes.read();
-    dbForQrCodes.JsonData = dbForQrCodes.data;
-    // dbForQrCodes.JsonData;
 
-    let LocalQrCheck = dbForQrCodes.JsonData.find(e => e.QrCodeId == LocalQrCodeId);
+    let LocalQrCheck = dbForQrCodes.find(e => e.QrCodeId == LocalQrCodeId);
 
     if (LocalQrCheck === undefined) {
         LocalReturnData.KReason = `No QrCode :${LocalQrCodeId}`
         return LocalReturnData;
     };
 
-
-    let LocalFactoryCheck = dbForQrCodes.JsonData.find(e => e.FactoryName == LocalFactoryName);
-
-    if (LocalFactoryCheck === undefined) {
+    if (LocalQrCheck.FactoryName !== LocalFactoryName) {
         LocalReturnData.KReason = `Not this Factory :${LocalFactoryName}`
         return LocalReturnData;
     };
