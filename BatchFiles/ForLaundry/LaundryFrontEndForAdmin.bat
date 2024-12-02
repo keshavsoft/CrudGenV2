41ea7944ddfe63d4db55453fa278a53b39241c97
pushpadamalanka82 <y*********@gmail.com>
2024-12-02 10:53:50 +0530

    batchfile

--- BatchFiles/ForLaundry/LaundryFrontEndForAdmin.bat
+++ BatchFiles/ForLaundry/LaundryFrontEndForAdmin.bat
@@ -11,6 +11,7 @@ call npm run AdminAllDays-CommonConfig-QrCode-BranchFilter
 call npm run AdminAllDays-CommonConfig-QrCodeAtStage-AsArray
 call npm run AdminAllDays-CommonConfig-QrCodeAtStage-BranchFilter
 call npm run AdminAllDays-CommonConfig-QrCodeAtStage-BranchWise
+call npm run Admin-Masters
 
 xcopy .\publicDir ..\CrudGenV2\public\Laundry\Admin /h /i /c /k /e /r /y
 
