b097d44395590b3f8b1027a4cb67be45993d6a2d
keshavsoft <nknnkumar@yahoo.com>
2024-12-02 16:41:47 +0530

    ScanV1 started

--- Custom/Cleaning/Branch/Factory/ToFactory/routes.js
+++ Custom/Cleaning/Branch/Factory/ToFactory/routes.js
@@ -3,9 +3,11 @@ import express from 'express';
 var router = express.Router();
 
 import { router as routerFromScan } from './Scan/routes.js';
+import { router as routerFromScanV1 } from './ScanV1/routes.js';
 import { router as routerFromVoucher } from './Voucher/routes.js';
 
 router.use('/Scan', routerFromScan);
+router.use('/ScanV1', routerFromScanV1);
 router.use('/Voucher', routerFromVoucher);
 
 export { router };
