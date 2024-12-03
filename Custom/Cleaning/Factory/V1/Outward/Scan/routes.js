7cbf3ac3ce0d168d3e765bea4fc06cf7ade9b8e3
pushpadamalanka82 <y*********@gmail.com>
2024-12-03 17:23:25 +0530

    endpoints in completion

--- Custom/Cleaning/Factory/V1/Outward/Scan/routes.js
+++ Custom/Cleaning/Factory/V1/Outward/Scan/routes.js
@@ -2,10 +2,10 @@ import express from 'express';
 
 var router = express.Router();
 
-// import { router as routerFromQrCode } from './QrCode/routes.js';
+import { router as routerFromQrCode } from './QrCode/routes.js';
 import { router as routerFromVoucher } from './Voucher/routes.js';
 
-// router.use('/QrCode', routerFromQrCode);
+router.use('/QrCode', routerFromQrCode);
 router.use('/Voucher', routerFromVoucher);
 
 export { router };
