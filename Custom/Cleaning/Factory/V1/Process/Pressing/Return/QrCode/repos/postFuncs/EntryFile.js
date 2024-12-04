dec43ab6ba45d172ee7cabe15e67d4adf8d223fc
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2024-12-04 17:19:40 +0530

    Keys

--- Custom/Cleaning/Factory/V1/Process/Pressing/Return/QrCode/repos/postFuncs/EntryFile.js
+++ Custom/Cleaning/Factory/V1/Process/Pressing/Return/QrCode/repos/postFuncs/EntryFile.js
@@ -2,8 +2,8 @@ import {
     PostFunc as PostFuncDal
 } from '../../dals/postFuncs/EntryFile.js';
 
-let PostFunc = ({ inFactory, inDataInsert }) => {
-    return PostFuncDal({ inFactory, inDataInsert });
+let PostFunc = ({ inFactory, inDataInsert, inQrCodeId, inVoucherRef }) => {
+    return PostFuncDal({ inFactory, inDataInsert,inQrCodeId, inVoucherRef });
 };
 
 export {
