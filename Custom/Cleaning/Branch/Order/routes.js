f3bc4646bf39f6d8966fcdefc903029526abc84a
Srivasu215 <sri.vasu215@gmail.com>
2024-12-11 19:24:46 +0530

    Delivey Start

--- Custom/Cleaning/Branch/Order/routes.js
+++ Custom/Cleaning/Branch/Order/routes.js
@@ -4,7 +4,9 @@ var router = express.Router();
 
 
 import { router as NewOrder } from './NewOrder/routes.js';
+import { router as Delivery } from './Delivery/routes.js';
 
 router.use('/NewOrder', NewOrder);
+router.use('/Delivery', Delivery);
 
 export { router };
